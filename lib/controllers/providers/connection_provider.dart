396db8e4ef7a2500618353a2897d8cc8baadcc7b
pushti0112 <71177321+pushti0112@users.noreply.github.com>
2022-09-24 22:27:29 -0400

    Update connection_provider.dart

--- lib/controllers/providers/connection_provider.dart
+++ lib/controllers/providers/connection_provider.dart
@@ -1,1 +1,33 @@
+import 'dart:async';
 
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:profile_details/utils/my_print.dart';
+
+class ConnectionProvider extends ChangeNotifier {
+  bool isInternet = true;
+  StreamSubscription<ConnectivityResult>? subscription;
+
+  ConnectionProvider() {
+    try {
+      MyPrint.printOnConsole("Connection Subscription Started");
+      subscription = Connectivity().onConnectivityChanged.listen((ConnectivityResult result) {
+        isInternet = result == ConnectivityResult.none ? false : true;
+        MyPrint.printOnConsole("Connection Status Changed:$isInternet");
+        notifyListeners();
+      });
+    }
+    catch (E) {
+      MyPrint.printOnConsole("Error in Connectivity Subscription:  $E");
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    if(subscription != null) {
+      subscription!.cancel();
+      subscription = null;
+    }
+  }
+}
